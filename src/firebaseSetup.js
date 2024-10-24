699fb713c26ce1337f820c7797cef3106f7cba6c
Tristan Greene <greene.tristan04@gmail.com>
2024-10-24 15:46:39 +0100

    Firebase SDK app and Firestore working

---
+++ src/firebaseSetup.js
@@ -0,0 +1,24 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDla9VlcegBM2_Aja0QiYNRh5FPhkJYVdA",
+  
+    authDomain: "sound-of-peanuts-cooking-site.firebaseapp.com",
+  
+    projectId: "sound-of-peanuts-cooking-site",
+  
+    storageBucket: "sound-of-peanuts-cooking-site.appspot.com",
+  
+    messagingSenderId: "673148647334",
+  
+    appId: "1:673148647334:web:2ee44e6f6210134a518167",
+  
+    measurementId: "G-XX6V4GR026"
+  
+  };
+  
+  // Initialize Firebase
+  
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
