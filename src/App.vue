25f644d307c23e1c982e6eea4124ac406f29668f
Jakub Chabowski <J.Chabowski1@universityofgalway.ie>
2025-02-03 14:36:46 +0000

    Added framework for Sidebar on Search Page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
 import Header from './components/Header.vue';
+import Sidebar from './components/Sidebar.vue';
 
 </script>
 
 <template>
   <Header />
-  <router-view>
-  </router-view>
+   <router-view>
+   </router-view>
 </template>
 
 <style scoped>
--- src/pages/Search.vue
+++ src/pages/Search.vue
@@ -1,4 +1,18 @@
 <template>
-    <p>Search</p>
-    <input type="text">
+  <div class = "container-fluid">
+    <div class = 'row'>
+      <div class="col-2">
+        <Sidebar />
+      </div>
+      <div class="col-9">
+        <p>Search</p>
+       <input type="text">
+      </div>
+      <div class="col-1"></div>
+    </div>
+    <div class = 'row'>the bottom</div>
+  </div>
 </template>
+<script setup lang="ts">
+import Sidebar from "@/components/Sidebar.vue";
+</script>
