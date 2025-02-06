bdaf0162141ceb2457343fd0f0eed5251894750b
Avery O'hEocha <avheocha@gmail.com>
2025-02-06 10:42:52 +0000

    Improve Recipe page

--- src/pages/Recipe.vue
+++ src/pages/Recipe.vue
@@ -1,24 +1,24 @@
 <script setup>
 import Sidebar from '../components/Sidebar.vue';
+import IngredientList from '@/components/IngredientList.vue';
 </script>
 
 <template>
-<div class = "container-fluid bg-secondary min-vh-100 vh-100">
+<div class = "container-fluid bg-secondary min-vh-100 vh-100" style="padding-top: 10px;">
     <div class = 'row d-flex align-items-start'>
       <div class="col-md-2">
         <Sidebar />
       </div>
       <div class="col-md-4">
-        <p>A description of the recipe here.</p>
-        <ul>
-            <li>Spicy!</li>
-            <li>Acidic</li>
-            <li>Salty</li>
-        </ul>    
+        
+        <IngredientList></IngredientList>   
       </div>
       <div class="col-md-5 d-flex justify-content-center align-items-center" style="background-color: lightblue; padding: 20px; border-radius: 15px;">
         <img  class="img-fluid rounded w-50 h-auto" src="..\assets\images\coconut.png" alt="Recipe default">
       </div>
+      
+        
+      
     </div>
   </div>
     
