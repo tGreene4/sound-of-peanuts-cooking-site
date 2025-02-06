fa3d1a2564b24c7ad10afff4146d01279c3fe1c5
Tristan Greene <greene.tristan04@gmail.com>
2025-02-06 10:46:54 +0000

    Added more API imports, added User.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,36 +1,13 @@
 <script>
-import app from '../api/firebase';
-import { getFunctions, httpsCallable } from 'firebase/functions';
+import {app,funcs} from '../api/firebase';
+import { httpsCallable } from 'firebase/functions';
 
-export default {
-    data() {
-        return {
-            handle: '',
-            comment: ''
-        }
-    },
-    methods: {
-        async postComment() {
-            console.log(this.handle);
-            console.log(this.comment);
-            const functions = getFunctions(app);
-            const postComment = httpsCallable(functions, 'postcomment');
-            const result = await postComment({
-                handle: this.handle,
-                comment: this.comment
-            });
-            console.log(result);
-        }
-    }
-}
 </script>
 
 <script setup>
 import MainMediaContainer from '../components/HomeMainMediaContainer.vue';
-import postComment from '../components/postComment.vue'
 </script>
 
 <template>
     <MainMediaContainer />
-    <postComment />
 </template>
---
+++ src/pages/User.vue
@@ -0,0 +1,16 @@
+<script>
+    import auth from '../api/firebase'
+    import {onAuthStateChanged} from 'firebase/auth'
+
+    onAuthStateChanged(auth, (user) => {
+        if(user){
+            user.uid
+        }else{
+            
+        }
+    })
+</script>
+
+<template @onload="">
+
+</template>
