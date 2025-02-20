c23f7f54b5796e48ea539c68682f9336a1761462
Tristan Greene <greene.tristan04@gmail.com>
2025-02-20 11:29:55 +0000

    Moved API functions to functions/index.js

--- src/api/dbUserRequestApi.js
+++ src/api/dbUserRequestApi.js
@@ -5,15 +5,4 @@ const { getFirestore } = require('firebase-admin/firestore');
 const db = getFirestore();
 initializeApp();
 
-exports.getDbUser = onRequest(async(req,res)=>{
 
-    const snapshot = await db.collection('User').where('uId','==',req.query.uid).limit(1).get();
-
-    if(snapshot.empty){
-        res.send('Error:no user with this UID')
-
-    }else{
-        res.json(snapshot.data())
-
-    }
-})
