10fa8a02a643121e1e5b7469e0e8a3e60b467467
Tristan Greene <greene.tristan04@gmail.com>
2025-03-11 14:13:59 +0000

    API improvements

--- functions/index.js
+++ functions/index.js
@@ -7,16 +7,18 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {limit,getDocs,orderBy} from 'firebase/firestore';
-import { uploadBytes } from 'firebase/storage';
+const {limit,getDocs,orderBy} = require('firebase/firestore');
+const { uploadBytes } = require('firebase/storage');
 const {onCall} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 const { initializeApp } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
-const { getStorage,getDownloadURL, } = require('firebase-admin/storage')
+const { getStorage,getDownloadURL, } = require('firebase-admin/storage');
+//const { default: Recipe } = require('@/pages/Recipe.vue');
+initializeApp({storageBucket: 'sound-of-peanuts-cooking-site.appspot.com'});
 const db = getFirestore();
 const store = getStorage();
-initializeApp({storageBucket: 'sound-of-peanuts-cooking-site.appspot.com'});
+
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
@@ -28,56 +30,68 @@ exports.getDbRecipes = onCall(async(req,res)=>{
     logger.info("Requested posts",req);
     
     if(snapshot.empty){
-        res.send('Error:no posts found')
+        return{success:false,message:"No recipes found"}
 
     }else{
-        res.json(snapshot.data())
+        const recipes = [];
+        snapshot.docs.forEach(doc =>{
+            recipes.push({
+                id:doc.id,
+                ...doc.data()
+            })
+        })
+        return recipes
 
     }
 })
 
 exports.getDbRecipeSingle = onCall(async(req,res)=>{
-    const q = query(db.collection("Recipes"),where('recipeId','==',req.query.rId).limit(1).get())
-    const snapshot = await(getDocs(q));
+    const id = req.data
+    const snapshot = await db.doc("/Recipe/"+id).get();
 
     if(snapshot.empty){
-        res.send('Error:no recipe found')
+        return{success:false,message:"Error: recipe not found"}
     }
     else{
-        res.json(snapshot.data())
+        return{success: true,recipe:snapshot.data()}
     }
 
 })
 
 exports.getDbUser = onCall(async(req,res)=>{
+    const {uID} = req.data
+    if(!uID){
+        throw new Error("UID not found")
+    }
 
     const snapshot = await db.collection('User').where('uId','==',req.query.uId).limit(1).get();
 
     if(snapshot.empty){
-        res.send('Error:no user with this UID')
+        return{success:false,message:'Error:no user with this UID'}
 
     }else{
-        res.json(snapshot.data())
+        return{success:true,user:snapshot.data()}
 
     }
 })
 
 exports.createDbUser = onCall(async(req,res)=>{
-    pfpRef = ref(store,req.query.uId+"/pfp.png")
-    uploadBytes(pfpRef,req.query.pfpFile).then((snapshot)=>{
+    const{uName,pfpFile,uId} = req.data
+    pfpRef = ref(store,uId+"/pfp.png")
+    uploadBytes(pfpRef,pfpFile).then((snapshot)=>{
         pfpDownloadURL = getDownloadURL(snapshot.ref);
     })
 
     const complete = await db.collection('User').add({
-        name: req.query.uName,
+        name: uName,
         pfpUrl: pfpDownloadURL,
-        uId: req.query.uId
+        uId: uId
     })
 
     if(complete){
-        res.send("User added")
+        return{success:true,message:"User added"}
     }else{
-        res.send("Error: could not create user")
+        return{success:false,message:"Error: could not create user"}
     }
 
 })
