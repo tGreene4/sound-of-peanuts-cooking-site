7b0ca88a292065392f6be6f66129df83651d76a2
Jakub Chabowski <J.Chabowski1@universityofgalway.ie>
2025-03-11 13:35:34 +0000

    Minor Changes to Create Recipe. Added Delete Button, cleaned up formatting a bit.

--- src/pages/CreateRecipe.vue
+++ src/pages/CreateRecipe.vue
@@ -1,3 +1,8 @@
+<script setup>
+import { getFunctions, httpsCallable } from "firebase/functions";
+import funcs from '../api/firebase';
+</script>
+
 <template>
   <div class = "flex-d flex-column align-self-center" id = "flexWrapper">
     <form class = "align-self-center" id = "content">
@@ -5,48 +10,73 @@
       <input class="form-control" v-model="name" id="nameField">
       <div class="container-fluid align-self-center">
         <div class="row">
-          <div class="col-sm-6">
+
+          <div class="col-lg-6">
             <label for = "instructionsField" class = "form-label">Instructions</label>
             <div class="container-fluid"   id="instructionsFields" v-for="(step,index) in steps">
               <div class= "row">
-                <div class="col-md-1"><a>{{index+1}}</a></div>
-                <div class="col-md-11"><input id="field" class="form-label" v-model="step.value"><br></div>
+                <div class="col-sm-1"><a>Step {{index+1}}</a></div>
+                <div class="col-sm-10">
+                  <input id="field" class="form-label" v-model="step.value">
+                </div>
+                <div class="col-sm-1">
+                  <button type="button" @click="deleteField('steps',index)">X</button>
+                </div>
               </div>
             </div>
-            <br><button type="button" @click="addStep">Add new step</button><br>
+            <br><button type="button" @click="addField('steps')">Add new step</button><br>
           </div>
-          <div class="col-sm-3">
+
+          <div class="col-lg-3">
             <label for = "ingredientFields" class = "form-label">Ingredients</label>
             <div id="ingredientFields" v-for="ingredient in ingredients">
-              <input id="field" class = "form-label" v-model="ingredient.value"><br>
+              <div class="row">
+                <div class="col-sm-11">
+                  <input id="field" class = "form-label" v-model="ingredient.value">
+                </div>
+                <div class="col-sm-1">
+                  <button type="button" @click="deleteField('ingredients',index)">X</button>
+                </div>
+              </div>
+              <br>
             </div>
-            <br><button type="button" @click="addIngredient">Add new Ingredient</button><br>
+            <br><button type="button" @click="addField('ingredients')">Add new Ingredient</button><br>
           </div>
-          <div class="col-sm-3">
+
+          <div class="col-lg-3">
             <label for = "equipmentFields" class = "form-label">Equipment</label>
             <div id="equipmentFields" v-for="item in equipment">
-              <input id="field" class = "form-label" v-model="item.value"><br>
+              <div class="row">
+                <div class="col-sm-11">
+                  <input id="field" class = "form-label" v-model="item.value">
+                </div>
+                <div class="col-sm-1">
+                  <button type="button" @click="deleteField('equipment',index)">X</button>
+                </div>
+              </div>
+              <br>
             </div>
-            <br><button type="button" @click="addEquipment">Add new Equipment</button><br>
+            <br><button type="button" @click="addField('equipment')">Add new Equipment</button><br>
           </div>
         </div>
       </div>
+
       <!--Basic, doesn't work yet-->
       <br><a>Upload Image</a><br>
       <input type="file" id="media" accept="image/*" multiple @change="(event) => handelFileUpload(event)"/>
       <br><br>
       <label for = "timeField" class = "form-label">Time</label>
       <input class="form-control" v-model="time"  id="timeField">
-      <div style="width:40%;align-self:center">
+      <br>
+      <div style="width:40%;align-self:center;">
         <button class="form-control" type="button" @click="postRecipe">Save Recipe</button>
       </div>
+      <br>
     </form>
   </div>
 </template>
 
 <script>
-import funcs from '../api/firebase';
-import { getFunctions, httpsCallable } from "firebase/functions";
 export default {
   data() {
     return {
@@ -57,20 +87,42 @@ export default {
       time: '',
     }
   },
+  mounted() {
+    this.ingredients.push({value: ''});
+    this.equipment.push({value: ''});
+    this.steps.push({value: ''});
+  },
   methods : {
-    //Method that adds a new input field to the given div
-    addIngredient: function (){
-      this.ingredients.push({value: ''});
+    addField: function(type){
+      switch(type){
+        case('ingredients'):
+          this.ingredients.push({value: ''});
+          break;
+        case('equipment'):
+          this.equipment.push({value: ''});
+          break;
+        case('steps'):
+          this.steps.push({value: ''});
+          break;
+      }
     },
-    addEquipment: function (){
-      this.equipment.push({value: ''});
-    },
-    addStep: function (){
-      this.steps.push({value: ''});
+    deleteField: function(type, index){
+      switch(type){
+        case('ingredients'):
+          this.ingredients.splice(index, 1);
+          break;
+        case('equipment'):
+          this.equipment.splice(index, 1);
+          break;
+        case('steps'):
+          this.steps.splice(index, 1);
+          break;
+      }
     },
     handleFileUpload: function (event) {
       console.log("image uploaded");
     },
+
     //Method that hopefully uploads the recipe info to the db
     async postRecipe() {
       console.log(this.name);
@@ -78,7 +130,7 @@ export default {
       console.log(this.equipment);
       console.log(this.steps );
       console.log(this.time);
-      /* Commented out for now
+      /* Code for posting recipe unfinished
       const postRecipe = httpsCallable(funcs, 'postrecipe');
       const result = await postRecipe(
           {
@@ -93,7 +145,7 @@ export default {
             "recipeId": 0,
           });
       console.log(result);
-       */
+      */
     }
   }
 }
