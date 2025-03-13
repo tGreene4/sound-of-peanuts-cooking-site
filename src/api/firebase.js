ded90ce9ff3f336d8729fe9df95dfb0f38c6b2ea
Conor Sheridan <c.sheridan30@universityofgalway.ie>
2025-03-13 16:36:34 +0000

    Removed incorrect log statement

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -29,9 +29,6 @@ const storage = getStorage(app);
 const functions = getFunctions(app);
 
 // Connect to the local emulator if we are running in a local environment
-console.log('Attempting to connect to the local emulator');
-console.log('Hostname:', window.location.hostname);
-
 if (window.location.hostname === '127.0.0.1') {
   console.log('Connecting to the local emulator');
   connectFunctionsEmulator(functions, 'localhost', 5001);
