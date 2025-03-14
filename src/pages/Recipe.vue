50205587492e487ebaa2d7a715ec0a43ccaafeaa
Conor Sheridan <c.sheridan30@universityofgalway.ie>
2025-03-14 09:32:42 +0000

    Added getRecipe basic functionality to Recipe.vue

--- src/pages/Recipe.vue
+++ src/pages/Recipe.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { functions } from '../api/firebase';
 import { httpsCallable } from 'firebase/functions';
+import { ref } from 'vue';
 
 import Sidebar from '../components/Sidebar.vue';
 import IngredientList from '@/components/IngredientList.vue';
@@ -16,6 +17,18 @@ const getHelloWorld = async () => {
   }
 };
 
+const recipeId = ref('');
+
+const getDbRecipeSingle = async () => {
+  console.log("Calling getDbRecipeSingle with ID:", recipeId.value);
+  const getDbRecipeSingleFunction = httpsCallable(functions, 'getDbRecipeSingle');
+  try {
+    const result = await getDbRecipeSingleFunction({ id: recipeId.value });
+    console.log(result.data);
+  } catch (error) {
+    console.error("Error calling getDbRecipeSingle:", error);
+  }
+};
 </script>
 
 <template>
@@ -30,7 +43,11 @@ const getHelloWorld = async () => {
       <div class="col-md-5 d-flex justify-content-center align-items-center" style="background-color: lightblue; padding: 20px; border-radius: 15px;">
         <img class="img-fluid rounded w-50 h-auto" src="..\assets\images\coconut.png" alt="Recipe default">
       </div>
+      
       <button class="btn btn-primary" @click="getHelloWorld">Hello world</button>
+
+      <input v-model="recipeId" placeholder="Enter Recipe ID" class="form-control mb-2" />
+      <button class="btn btn-primary" @click="getDbRecipeSingle">Get Recipe</button>    
     </div>
   </div>
 </template>
