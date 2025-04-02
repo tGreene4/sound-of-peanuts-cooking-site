24c264eaadcf1c8d400439cbed4b59c9027f72e2
Jakub Chabowski <J.Chabowski1@universityofgalway.ie>
2025-04-02 14:27:15 +0100

    Changes for mobile scaling

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,9 @@ import Header from './components/Header.vue';
 @import './assets/main.css';
 #body {
   min-height: 100%;
+  height: 100%;
   min-width: 720px;
+  width: 100%;
 }
 
 .main-content {
